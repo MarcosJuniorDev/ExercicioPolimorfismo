4f3f1d0cbb7ba09df819b2411c64294f2ef9c4e5
Marcos Junior Oliveira <marcosjoliveiradev@gmail.com>
2023-04-18 11:00:20 -0300

    Exercicio proposto de polimorfismo em c#

---
+++ ExercicioPoli/Program.cs
@@ -0,0 +1,46 @@
+﻿using ExercicioPoli.Entities;
+using System.Collections.Generic;
+using System.Globalization;
+
+namespace ExercicioPoli
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter the number of Employee: ");
+            int n = int.Parse(Console.ReadLine());
+            List<Employee> employees = new List<Employee>();
+
+            for (int i = 1; i <= n; i++)
+            {
+                Console.WriteLine($"Employee #{i} data:");
+                Console.Write("Outsourced (y/n)? ");
+                char outsorced = char.Parse(Console.ReadLine());
+                Console.Write("Name: ");
+                string name = Console.ReadLine();
+                Console.Write("Hours: ");
+                int hours = int.Parse(Console.ReadLine());
+                Console.Write("Value Per Hour: ");
+                double valuePerHour = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+                if (outsorced == 'y' || outsorced == 'Y')
+                {
+                    Console.Write("Additional charge: ");
+                    double additionalCharge = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+                    employees.Add(new OutsourcedEmployee(name, hours, valuePerHour, additionalCharge));
+                }
+                else
+                {
+                    employees.Add(new Employee(name, hours, valuePerHour));
+                }
+                Console.WriteLine();
+            }
+            Console.WriteLine();
+            Console.WriteLine("PAYMENTS:");
+            foreach (Employee emp in employees)
+            {
+                Console.WriteLine($"{emp.Name} - $ {emp.Payment().ToString("F2", CultureInfo.InvariantCulture)}");
+            }
+        }
+    }
+}
